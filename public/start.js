be575fbbcad5714d63f2e93a6e0699491cd78934
KrashMello <joelenrikesoteldo@gmail.com>
2025-12-20 20:14:40 -0400

    feat: update functionality

--- public/start.js
+++ public/start.js
@@ -1,38 +1,74 @@
+
+let data_object = JSON.parse('{}')
+document.addEventListener('DOMContentLoaded', async function () {
+  const object = document.getElementById('object-text-area')
+  data_object = JSON.parse(object.value)
+  object.addEventListener('input', function (e) {
+    data_object = JSON.parse(e.target.value)
+  })
+})
+
 const elements = {
   row: () => {
-    let element = document.createElement('div')
-    element.id = crypto.randomUUID()
-    element.classList.add('bg-slate-500', 'text-white', 'px-4', 'py-2', 'rounded-md', 'flex-row', 'gap-1', 'flex', 'w-full', 'min-h-32')
-    element.ondrop = dropHandler
-    element.ondragover = dragoverHandler
-    element.ondragenter = dragEnterHandler
-    element.ondragleave = dragLeaveHandler
-    element.ondragstart = dragStartInElementHandler
-    element.draggable = true
-    return element
+    const class_list = ['bg-slate-500', 'text-white', 'px-4', 'py-2', 'rounded-md', 'flex-row', 'gap-1', 'flex', 'w-full', 'min-h-32']
+    const element_text = `<div
+id="${crypto.randomUUID()}" 
+class="${class_list.join(' ')}"
+ondrop="dropHandler(event)"
+ondragover="dragoverHandler(event)"
+ondragenter="dragEnterHandler(event)"
+ondragleave="dragLeaveHandler(event)"
+ondragstart="dragStartInElementHandler(event)"
+draggable="true"
+>
+</div>
+`
+    return element_text
   },
   col: () => {
-    let element = document.createElement('div')
-    element.id = crypto.randomUUID()
-    element.classList.add('bg-slate-300', 'text-white', 'px-4', 'py-2', 'rounded-md', 'flex-col', 'gap-1', 'flex', 'w-full', 'min-h-32')
-    element.ondrop = dropHandler
-    element.ondragover = dragoverHandler
-    element.ondragenter = dragEnterHandler
-    element.ondragleave = dragLeaveHandler
-    element.ondragstart = dragStartInElementHandler
-    element.draggable = true
-    return element
+    const class_list = ['bg-slate-300', 'text-white', 'px-4', 'py-2', 'rounded-md', 'flex-col', 'gap-1', 'flex', 'w-full', 'min-h-32']
+    const element_text = `<div
+id="${crypto.randomUUID()}" 
+class="${class_list.join(' ')}"
+ondrop="dropHandler(event)"
+ondragover="dragoverHandler(event)"
+ondragenter="dragEnterHandler(event)"
+ondragleave="dragLeaveHandler(event)"
+ondragstart="dragStartInElementHandler(event)"
+draggable="true"
+>
+</div>
+`
+    return element_text
+  },
+  paragraph: () => {
+    const class_list = ['bg-slate-300', 'text-white', 'px-4', 'py-2', 'rounded-md', 'flex-col', 'gap-1', 'flex', 'w-full', 'min-h-32']
+    const element_text = `<div
+id="${crypto.randomUUID()}" 
+data-key="title"
+data-value="paragraph"
+class="${class_list.join(' ')}"
+ondrop="dropHandler(event)"
+ondragover="dragoverHandler(event)"
+ondragenter="dragEnterHandler(event)"
+ondragleave="dragLeaveHandler(event)"
+ondragstart="dragStartInElementHandler(event)"
+onclick="paragraphSelected(event)"
+draggable="true"
+>
+{{title}}
+</div>
+`
+    return element_text
   }
 }
 let selected_element = null
+let frame_text = ''
 function dragstartHandler(ev) {
   const type = ev.target.id
-  console.log(ev.target.id)
   selected_element = elements[type]()
 }
 function dragStartInElementHandler(ev) {
-  ev.preventDefault();
-  console.log(ev.target.id)
   ev.dataTransfer.setData("element", ev.target.id);
 }
 function dragEnterHandler(ev) {
@@ -44,18 +80,64 @@ function dragLeaveHandler(ev) {
 function dragoverHandler(ev) {
   ev.preventDefault();
 }
-
 function dropHandler(ev) {
   ev.preventDefault();
   ev.target.classList.remove('border-2', 'border-orange-500')
+  let frame = document.getElementById('frame')
+  const parser = new DOMParser();
   if (selected_element != null) {
-    ev.target.appendChild(selected_element)
+    let doc = parser.parseFromString(selected_element, 'text/html');
+    let element = doc.body.firstElementChild
+    ev.target.appendChild(element)
     selected_element = null
-    return
-  }
-  if (ev.dataTransfer.getData("element") != '') {
-    console.log(ev.dataTransfer.getData("element"))
+  } else if (ev.dataTransfer.getData("element") != '') {
     ev.target.appendChild(document.getElementById(ev.dataTransfer.getData("element")))
-    return
   }
+}
+let show_preview = false
+function previewHandler(ev) {
+  ev.preventDefault();
+  show_preview = !show_preview
+  if (show_preview) {
+    let frame = document.getElementById('frame')
+    let preview = document.getElementById('preview')
+    frame.classList.add('hidden')
+    frame.classList.remove('flex')
+    preview.classList.remove('hidden')
+    preview.classList.add('flex')
+    const parser = new DOMParser();
+    const template = Handlebars.compile(frame.outerHTML)
+    let doc = parser.parseFromString(template(data_object), 'text/html');
+    let element = doc.body.firstElementChild
+
+    preview.replaceChildren(...element.childNodes)
+  } else {
+    frame.classList.remove('hidden')
+    frame.classList.add('flex')
+    preview.classList.add('hidden')
+    preview.classList.remove('flex')
+    preview.replaceChildren()
+  }
+}
+selected_element = null
+function paragraphSelected(ev) {
+  selected_element = ev.target
+  const var_key = document.getElementById('var-key')
+  const var_value = document.getElementById('var-value')
+  var_key.value = ev.target.dataset.key
+  var_value.value = ev.target.dataset.value
+}
+function saveVar(ev) {
+  ev.preventDefault();
+  const var_key = document.getElementById('var-key')
+  const var_value = document.getElementById('var-value')
+  data_object[var_key.value] = var_value.value
+  selected_element.innerText = `{{${var_key.value}}}`
+  var_key.value = ''
+  var_value.value = ''
+
+}
+function frameChange(ev) {
+  frame_text = ev.target.value
+  // console.log(frame_text)
 }
