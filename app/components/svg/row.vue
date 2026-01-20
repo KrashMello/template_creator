dd3f8e9b41a3d1e63bb9b961b785bb05de088143
krashmello <joelenrikesoteldo@gmail.com>
2026-01-20 14:47:03 -0400

    feat: update svg components

---
+++ app/components/svg/row.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16">
+    <path fill="currentColor"
+      d="M4.5 2A2.5 2.5 0 0 0 2 4.5v7A2.5 2.5 0 0 0 4.5 14h7a2.5 2.5 0 0 0 2.5-2.5v-7A2.5 2.5 0 0 0 11.5 2zM3 4.5A1.5 1.5 0 0 1 4.5 3h7A1.5 1.5 0 0 1 13 4.5V8H3z" />
+  </svg>
+</template>
