3108afd2305ee7dc91ef115895ddfb3f2f0699fc
KrashMello <joelenrikesoteldo@gmail.com>
2026-01-20 21:14:05 -0400

    feat: update meta and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   app: {
     head: {
-      title: 'Layout Builder',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/short-logo.png' }
+      ],
+      title: 'template creator',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
