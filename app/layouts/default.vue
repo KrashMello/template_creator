aed9d52b2982fcae775ba47faf5d93f9ac6a074b
krashmello <joelenrikesoteldo@gmail.com>
2026-01-29 15:18:00 -0400

    feat: remove gap in layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-slate-700 p-3 min-h-screen flex">
-    <div class="grid grid-cols-12 gap-2 flex-1">
+    <div class="grid grid-cols-12 flex-1">
       <slot />
     </div>
   </div>
