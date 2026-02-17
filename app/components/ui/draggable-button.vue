279595dcf04e47a545b7bbc9898da503d8e5889d
AdelGann <imadelg14@gmail.com>
2026-02-17 18:31:32 -0400

    refactor: center-container css styles and components

--- app/components/ui/draggable-button.vue
+++ app/components/ui/draggable-button.vue
@@ -1,33 +1,49 @@
 <template>
   <div
     class="componente w-full h-fit px-4 py-2 rounded-lg hover:border-blue-400 cursor-grab active:cursor-grabbing bg-gradient-to-r from-blue-50 to-indigo-50 transition-all draggable-component"
-    draggable="true" :data-tag="props.tag" :data-nombre="props.name" :data-data="JSON.stringify(props.data)"
-    @dragstart="onDragStart">
+    draggable="true"
+    :data-tag="props.tag"
+    :data-nombre="props.name"
+    :data-data="JSON.stringify(props.data)"
+    @dragstart="onDragStart"
+  >
     <div class="flex items-center flex-col">
       <component class="size-6 2xl:size-12 text-slate-500" :is="props.icon" />
       <div class="flex flex-col items-center text-xs 2xl:text-sm">
-        <div class="  font-medium text-slate-500"> {{ props.name }}</div>
-        <div class=" text-slate-400"> {{ props.tag }}
-        </div>
+        <div class="font-medium text-slate-500">{{ props.name }}</div>
+        <div class="text-slate-400">{{ props.tag }}</div>
       </div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 type propsType = {
-  name: string, icon: any, tag: string, data: {
-    table?: boolean, columns?: string[],
-    content?: string, class?: string, row?: string
-  }
-}
-const props = defineProps<propsType>()
+  name: string;
+  icon: any;
+  tag: string;
+  data: {
+    table?: boolean;
+    columns?: string[];
+    content?: string;
+    class?: string;
+    row?: string;
+  };
+};
+const props = defineProps<propsType>();
 const onDragStart = (event) => {
-  const tag = event.target.dataset.tag || event.target.closest('.draggable-component')?.dataset.tag
-  const data = event.target.dataset.data ? JSON.parse(event.target.dataset.data) : {}
-  const nombre = event.target.dataset.nombre
+  const tag =
+    event.target.dataset.tag ||
+    event.target.closest(".draggable-component")?.dataset.tag;
+  const data = event.target.dataset.data
+    ? JSON.parse(event.target.dataset.data)
+    : {};
+  const nombre = event.target.dataset.nombre;
 
-  event.dataTransfer.setData('text/plain', JSON.stringify({ tag, data, nombre }))
-  event.dataTransfer.effectAllowed = 'move'
-}
-
+  event.dataTransfer.setData(
+    "text/plain",
+    JSON.stringify({ tag, data, nombre }),
+  );
+  event.dataTransfer.effectAllowed = "move";
+};
 </script>
